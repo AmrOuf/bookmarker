39451e1bd510911e3db417d73993695305b14749
AmrOuf <amr.ouf96@gmail.com>
2019-07-15 18:49:50 +0200

    Update main JS file

--- js/plugins.js
+++ js/plugins.js
@@ -1,13 +1,30 @@
-function addSite()
+document.getElementById("myForm").addEventListener('submit', addSite);
+
+function addSite(e)
 {
+	e.preventDefault();
+
 	var siteName = document.getElementById("site-name");
 	var siteUrl = document.getElementById("site-url");
 
 	console.log(siteUrl.value);
 
+	if(siteName.value.length == 0)
+	{
+		alert("Please write your website name");
+		return;
+	}
+
+	if(siteUrl.value.length == 0)
+	{
+		alert("Please write a valid URL");
+		return;
+	}
+
 	document.getElementById("bookmarked").innerHTML += 
 		'<div class="website mt-4">'+
-			'<span class="site-name mr-3">'+siteName.value+'</span>'+
+			'<span class="site-name mr-3">'+siteName.value.charAt(0).toUpperCase()+
+			siteName.value.slice(1)+'</span>'+
 
 			'<button type="button" class="btn btn-success ml-1 id="site-visit">'+
 				'<a href="'+siteUrl.value+'" target="_blank">Visit</a>'+
